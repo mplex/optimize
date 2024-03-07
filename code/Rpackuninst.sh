48a65e17df2c4e7161b52019e5da6fd81e5ef228
Antonio Rivero Ostoic <multiplex@post.com>
2024-03-07 10:13:14 +0100

    Create Rpackuninst.sh
    uninstall R package from Jupyter Notebook at Docker, then install new version from GH repo

---
+++ code/Rpackuninst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt update && apt install -y --no-install-recommends software-properties-common dirmngr gnupg
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+R --no-save << EOF
+remove.packages("sdam")
+devtools::install_github("mplex/cedhar", subdir="pkg/sdam")
+EOF
