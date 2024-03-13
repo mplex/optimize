26db15994d52e24a427b5dcfd098905a747457ca
Antonio Rivero Ostoic <multiplex@post.com>
2024-03-13 15:08:38 +0100

    Create RpackinstDevOps.sh

---
+++ code/RpackinstDevOps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt update && apt install -y --no-install-recommends software-properties-common dirmngr gnupg
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+R --no-save << EOF
+remotes::install_git("https://vso-via-teamservices.visualstudio.com/Sandkasse-AOST/_git/viadw", git="external")
+EOF
