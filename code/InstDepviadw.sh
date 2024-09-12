89e2f9455d135797bcc2c4400917078b24c5d85c
AOST <AOST@via.dk>
2024-09-12 14:52:22 +0200

    update InstDepviadw

--- code/InstDepviadw.sh
+++ code/InstDepviadw.sh
@@ -4,8 +4,9 @@ apt update && apt install -y --no-install-recommends software-properties-common 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
-install.packages("httr")
-install.packages("rjson")
+install.packages("odbc")
+#install.packages("httr")
+#install.packages("rjson")
 #install.packages("RODBC")
 #install.packages("knitr, rmarkdown")
 EOF
