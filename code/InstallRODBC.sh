a9dffe1e8d589554894d0770801289138639094f
aost <AOST@via.dk>
2024-11-22 13:31:29 +0100

    update InstallRODBC

--- code/InstallRODBC.sh
+++ code/InstallRODBC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get install r-cran-rodbc
+apt-get install mssql-tools18 unixodbc-dev
 apt update && apt install -y --no-install-recommends software-properties-common dirmngr gnupg
-apt-get install unixodbc-dev
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
