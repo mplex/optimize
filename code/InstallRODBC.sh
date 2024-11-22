5709b419de70489ff4796f3bfe7795aa163381fd
aost <AOST@via.dk>
2024-11-22 13:27:33 +0100

    update InstallRODBC

--- code/InstallRODBC.sh
+++ code/InstallRODBC.sh
@@ -2,10 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get install r-cran-rodbc
 apt update && apt install -y --no-install-recommends software-properties-common dirmngr gnupg
-apt install unixodbc-dev
+apt-get install unixodbc-dev
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
 install.packages("odbc")
-install.packages("RODBC")
+#install.packages("RODBC")
 EOF
