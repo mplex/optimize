c75e40a21f6664225e126937ebbc32a56c583210
aost <AOST@via.dk>
2024-11-22 13:10:29 +0100

    update InstallRODBC

--- code/InstallRODBC.sh
+++ code/InstallRODBC.sh
@@ -5,5 +5,6 @@ apt update && apt install -y --no-install-recommends software-properties-common 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
+install.packages("odbc")
 install.packages("RODBC")
 EOF
