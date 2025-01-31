67d6a96537ac4b6146b787250cebfb816cbc7dd3
mplex <multiplex@post.com>
2025-01-31 09:29:00 +0100

    add PKI

--- code/InstDepviadw.sh
+++ code/InstDepviadw.sh
@@ -4,9 +4,10 @@ apt update && apt install -y --no-install-recommends software-properties-common 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
+install.packages("PKI")
 install.packages("odbc")
 #install.packages("httr")
-#install.packages("rjson")
-#install.packages("RODBC")
+install.packages("rjson")
+install.packages("RODBC")
 #install.packages("knitr, rmarkdown")
 EOF
