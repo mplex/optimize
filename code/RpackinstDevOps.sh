950c550f88c921dc8c1dc5df0ffbb5a8f48505cf
Antonio Rivero Ostoic <multiplex@post.com>
2025-03-10 12:31:36 +0100

    Update RpackinstDevOps.sh

--- code/RpackinstDevOps.sh
+++ code/RpackinstDevOps.sh
@@ -4,5 +4,5 @@ apt update && apt install -y --no-install-recommends software-properties-common 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 R --no-save << EOF
-remotes::install_git("https://vso-via-teamservices.visualstudio.com/Sandkasse-AOST/_git/viadw", git="external")
+remotes::install_git("https://vso-via-teamservices.visualstudio.com/Sandkasse-AOST/_git/viadw", git="external", ref="dev")
 EOF
