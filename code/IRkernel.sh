d0cf3c31da51e796c27b933b66b14735e9988562
Antonio Rivero Ostoic <multiplex@post.com>
2026-08-18 11:09:29 +0200

    Update IRkernel.sh

--- code/IRkernel.sh
+++ code/IRkernel.sh
@@ -5,7 +5,7 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee
 add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 #apt install -y r-base libxml2-dev libssl-dev libcurl4-openssl-dev libbz2-dev liblzma-dev libfontconfig1-dev libharfbuzz-dev libtiff-dev r-cran-tidyverse
 R --no-save << EOF
-install.packages("devtools")
+install.packages("devtools", repos="https://cloud.r-project.org/")
 devtools::install_github("IRkernel/IRkernel")
 IRkernel::installspec()
 EOF
